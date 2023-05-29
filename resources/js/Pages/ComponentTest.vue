71e1aba421aae5963f2406e07327fd0e4610c3e7
kiyomura10 <taiti199@yahoo.co.jp>
2023-05-29 21:38:14 +0900

    inertia-vue.js

---
+++ resources/js/Pages/ComponentTest.vue
@@ -0,0 +1,12 @@
+<script setup>
+import GuestLayout from '@/Layouts/GuestLayout.vue'
+import Label from '@/Components/InputLabel.vue'
+import Input from '@/Components/TextInput.vue'
+</script>
+
+<template>
+    
+    <GuestLayout>
+        なにか文字を書く
+    </GuestLayout>
+</template>
